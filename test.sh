420b26226a1e5035b02755701e81816683295458
michelbarnich <private@michelbarnich.com>
2021-10-13 19:03:07 +0200

    simple test sccipt, the magic word check needs to be commented out in the node for the test script to work!

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+BLOCK_QUEUE_LENGTH=29
+
+echo "running Test script"
+printf "generating and sending %d packages...\n" $BLOCK_QUEUE_LENGTH
+
+for i in $(eval echo "{0..$BLOCK_QUEUE_LENGTH}")
+do
+    printf "generating packet %d...\n" $i
+    touch ./.testpackage$i
+
+    echo -e 01 | xxd -r -p >> ./.testpackage$i
+    printf '%(%s)T' -1 >> ./.testpackage$i
+    hexdump -n 64 -e '16/4 "%08X"' /dev/urandom >> ./.testpackage$i
+    hexdump -n 64 -e '16/4 "%08X"' /dev/urandom >> ./.testpackage$i
+    hexdump -n 64 -e '16/4 "%08X" 1 "\0"' /dev/urandom >> ./.testpackage$i
+
+    printf "sending packet %d...\n" $i
+    netcat -w 1 127.0.0.1 50000 <./.testpackage$i &
+
+    printf "deleting packet %d...\n" $i
+    rm ./.testpackage$i
+done
