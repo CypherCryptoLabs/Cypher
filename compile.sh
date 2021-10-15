11983e96f49a282831c136eeb5ce20611bae73d9
michelbarnich <private@michelbarnich.com>
2021-10-15 11:53:22 +0200

    updated comile script

--- compile.sh
+++ compile.sh
@@ -9,18 +9,18 @@ fi
 
 if [ $1 = "test1" ]
 then
-    gcc ./src/tests/test1.c -o ./tests/test1
+    gcc ./src/tests/test1.c -o ./tests/test1 -lcrypto -lz $(mysql_config --libs)
 fi
 
 if [ $1 = "test2" ]
 then
-    gcc ./src/tests/test2.c -o ./tests/test2  -lcrypto
+    gcc ./src/tests/test2.c -o ./tests/test2  -lcrypto -lcrypto -lz $(mysql_config --libs)
 fi
 
 if [ $1 = "all" ]
 then
-    gcc ./src/tests/test1.c -o ./tests/test1
-    gcc ./src/tests/test2.c -o ./tests/test2  -lcrypto
+    gcc ./src/tests/test1.c -o ./tests/test1 -lcrypto -lz $(mysql_config --libs)
+    gcc ./src/tests/test2.c -o ./tests/test2   -lcrypto -lz $(mysql_config --libs)
     gcc ./src/node/main.c -pthread -o ./products/node -lcrypto -lz $(mysql_config --libs)
 fi
 
