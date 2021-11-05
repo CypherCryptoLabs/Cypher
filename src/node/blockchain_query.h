fcc0bf2372958e3e5a672aca82364c829c14dea0
michelbarnich <private@michelbarnich.com>
2021-11-05 16:10:21 +0000

    fix for isseu #15

--- src/node/blockchain_query.h
+++ src/node/blockchain_query.h
@@ -496,51 +496,25 @@ struct return_data register_new_node(char *ip_address, char *data_blob, int data
     char *query_string;
     MYSQL_BIND param_uoi[3];
 
-    if(num_rows < 1) {
+    query_string = "REPLACE INTO node VALUES(?, ?, ?);";
 
-        query_string = "INSERT INTO node(id, ip_address, public_key) VALUES(?, ?, ?);";
+    param_uoi[0].buffer_type = MYSQL_TYPE_VARCHAR;
+    param_uoi[0].buffer = decrypted_hash;
+    param_uoi[0].is_unsigned = 0;
+    param_uoi[0].is_null = 0;
+    param_uoi[0].length = &decrypted_size;
 
-        param_uoi[0].buffer_type = MYSQL_TYPE_VARCHAR;
-        param_uoi[0].buffer = decrypted_hash;
-        param_uoi[0].is_unsigned = 0;
-        param_uoi[0].is_null = 0;
-        param_uoi[0].length = &decrypted_size;
+    param_uoi[1].buffer_type = MYSQL_TYPE_VARCHAR;
+    param_uoi[1].buffer = ip_address;
+    param_uoi[1].is_unsigned = 0;
+    param_uoi[1].is_null = 0;
+    param_uoi[1].length = &ip_address_len;
 
-        param_uoi[1].buffer_type = MYSQL_TYPE_VARCHAR;
-        param_uoi[1].buffer = ip_address;
-        param_uoi[1].is_unsigned = 0;
-        param_uoi[1].is_null = 0;
-        param_uoi[1].length = &ip_address_len;
-
-        param_uoi[2].buffer_type = MYSQL_TYPE_VARCHAR;
-        param_uoi[2].buffer = pub_key;
-        param_uoi[2].is_unsigned = 0;
-        param_uoi[2].is_null = 0;
-        param_uoi[2].length = &pub_key_len;
-
-    } else {
-
-        query_string = "UPDATE node SET ip_address = ?, public_key = ? WHERE id = ?;";
-
-        param_uoi[0].buffer_type = MYSQL_TYPE_VARCHAR;
-        param_uoi[0].buffer = ip_address;
-        param_uoi[0].is_unsigned = 0;
-        param_uoi[0].is_null = 0;
-        param_uoi[0].length = &ip_address_len;
-
-        param_uoi[1].buffer_type = MYSQL_TYPE_VARCHAR;
-        param_uoi[1].buffer = pub_key;
-        param_uoi[1].is_unsigned = 0;
-        param_uoi[1].is_null = 0;
-        param_uoi[1].length = &pub_key_len;
-
-        param_uoi[3].buffer_type = MYSQL_TYPE_VARCHAR;
-        param_uoi[3].buffer = decrypted_hash;
-        param_uoi[3].is_unsigned = 0;
-        param_uoi[3].is_null = 0;
-        param_uoi[3].length = &decrypted_size;
-
-    }
+    param_uoi[2].buffer_type = MYSQL_TYPE_VARCHAR;
+    param_uoi[2].buffer = pub_key;
+    param_uoi[2].is_unsigned = 0;
+    param_uoi[2].is_null = 0;
+    param_uoi[2].length = &pub_key_len;
 
     MYSQL_STMT* update_or_insert_stmt = mysql_prepared_query(query_string, param_uoi, dbc);
     mysql_stmt_close(update_or_insert_stmt);
