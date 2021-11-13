fcb5dcaf5a6804d09e735b3adcff72d584c7509e
michelbarnich <private@michelbarnich.com>
2021-11-13 09:52:53 +0000

    I hate DHCP

--- tests/test.sh
+++ tests/test.sh
@@ -16,7 +16,7 @@ do
     hexdump -n 64 -e '16/4 "%08X" 1 "\0"' /dev/urandom >> ./.testpackage$i
 
     printf "sending packet %d...\n" $i
-    netcat -w 1 192.168.2.134 50000 <./.testpackage$i &
+    netcat -w 1 192.168.2.104 50000 <./.testpackage$i &
 
     printf "deleting packet %d...\n" $i
     rm ./.testpackage$i
