6974ed816de08105291a9dfa619dc7a2df760eff
michelbarnich <private@michelbarnich.com>
2022-03-14 22:02:13 +0000

    readability improvements in main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
 // CONFIG //
-var port = 0; 
-var host = ''; 
-const fs = require("fs");
 
 const transactionQueue = require(__dirname + "/transactionQueue.js");
 const bcrypto = require(__dirname + "/bcrypto.js");
 const networking = require(__dirname + "/networking.js");
+const fs = require("fs");
 
-var configFile = fs.readFileSync("config.json", "utf-8");
-host = JSON.parse(configFile).host;
-port = JSON.parse(configFile).port;
-stableNode = JSON.parse(configFile).stableNode;
-
+let config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
 var BCrypto = new bcrypto();
 const TransactionQueue = new transactionQueue(1235, BCrypto);
+let netInstance = new networking(config.host, config.port, BCrypto, config.stableNode);
 
-let netInstance = new networking(host, port, BCrypto,stableNode);
 TransactionQueue.worker(netInstance);
 netInstance.connectionHandler();
