288245eea41066b9ae8087f50af5b7abd1bd37ec
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2022-08-13 15:59:17 +0200

    disable network checks

--- src/blockchain.js
+++ src/blockchain.js
@@ -330,7 +330,7 @@ class blockchain {
       if(JSON.stringify(Object.getOwnPropertyNames(block.networkDiff)) != JSON.stringify(["registered", "left"]))
          return false;
       
-      if(block.networkDiff.registered.length != networkDiff.registered.length || block.networkDiff.left.length != networkDiff.left.length)
+      /*if(block.networkDiff.registered.length != networkDiff.registered.length || block.networkDiff.left.length != networkDiff.left.length)
          return false;
 
       for(var i = 0; i < block.networkDiff.registered.length; i++) {
@@ -361,7 +361,7 @@ class blockchain {
 
          if(!leaveFound)
             return false;
-      }
+      }*/
 
       var expectedRewardAmount = 0;
 
