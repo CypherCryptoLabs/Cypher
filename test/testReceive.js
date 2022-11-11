91e9c7047dcfaed94d3a7e353a3518df84a64168
michelbarnich <private@michelbarnich.com>
2022-11-11 19:30:38 +0000

    proof of reception in test script

--- test/testReceive.js
+++ test/testReceive.js
@@ -44,44 +44,112 @@ function signPacket(packet) {
     }
 }
 
-client.connect(PORT, HOST, () => { 
-    console.log(`client connected to ${HOST}:${PORT}`); 
-    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client  
+var receivedData;
+let requestFinished = new Promise((resolve, reject) => {
+    client.connect(PORT, HOST, () => { 
+        console.log(`client connected to ${HOST}:${PORT}`); 
+        // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client  
+    
+        var createTransactionPacket = {
+            queryID : 8,
+            unixTimestamp : Date.now(),
+            payload : {
+                type: "retrieve"
+            },
+            publicKey : publicKey.toPem(),
+            signature : ""
+        };
+    
+        var createTransactionPacketForSignature = JSON.parse(JSON.stringify(createTransactionPacket));
+        delete createTransactionPacketForSignature.queryID;
+        delete createTransactionPacketForSignature.signature;
+    
+        var createTransactionPacketJSON = JSON.stringify(createTransactionPacketForSignature);
+    
+        createTransactionPacket.signature = signPacket(createTransactionPacketJSON).toBase64();
+    
+        client.write(JSON.stringify(createTransactionPacket)); 
+        client.end();
+    });
+    
+    client.on('data', (data) => {     
+        receivedData = data.toString("utf8").slice(0, -1);;
+        resolve();
+        if (data.toString().endsWith('exit')) { 
+            client.destroy(); 
+        }
+    }); 
+    
+    // Add a 'close' event handler for the client socket 
+    client.on('close', () => { 
+        console.log('Client closed'); 
+    }); 
+    
+    client.on('error', (err) => { 
+        reject();
+        console.error(err); 
+    }); 
+}).then((async () => {
+    console.log(`Client received: ${receivedData}`);
+    console.log(receivedData.length)
+    var receivedMessages = JSON.parse(receivedData).payload;
 
-    var createTransactionPacket = {
-        queryID : 8,
-        unixTimestamp : Date.now(),
-        payload : {
-            type: "retrieve"
-        },
-        publicKey : publicKey.toPem(),
-        signature : ""
-    };
+    let keys = Object.keys(receivedMessages)
 
-    var createTransactionPacketForSignature = JSON.parse(JSON.stringify(createTransactionPacket));
-    delete createTransactionPacketForSignature.queryID;
-    delete createTransactionPacketForSignature.signature;
+    for (var i = 0; i < keys.length; i++) { 
+        let key = keys[i];
+        var tmpclient = new net.Socket();
+        
+        let dataTransmitted = new Promise((resolve, reject) => {
+            tmpclient.connect(PORT, HOST, () => { 
+                console.log(`client connected to ${HOST}:${PORT}`); 
+                // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client  
+                console.log(key)
+                let messageHash = crypto.createHash('sha256').update(JSON.stringify(receivedMessages[key])).digest('hex')
+                var createTransactionPacket = {
+                    queryID : 9,
+                    unixTimestamp : Date.now(),
+                    payload : {
+                    },
+                    publicKey : publicKey.toPem(),
+                    signature : ""
+                };
+                
+                createTransactionPacket.payload[messageHash] = signPacket(messageHash).toBase64();
+                console.log(createTransactionPacket)
+            
+                var createTransactionPacketForSignature = JSON.parse(JSON.stringify(createTransactionPacket));
+                delete createTransactionPacketForSignature.queryID;
+                delete createTransactionPacketForSignature.signature;
+            
+                var createTransactionPacketJSON = JSON.stringify(createTransactionPacketForSignature);
+            
+                createTransactionPacket.signature = signPacket(createTransactionPacketJSON).toBase64();
+            
+                tmpclient.write(JSON.stringify(createTransactionPacket)); 
+                tmpclient.end();
+            });
+            
+            tmpclient.on('data', (data) => {     
+                console.log(data.toString())
+                resolve()
+                if (data.toString().endsWith('exit')) { 
+                    tmpclient.destroy(); 
+                }
+            }); 
+            
+            // Add a 'close' event handler for the client socket 
+            tmpclient.on('close', () => { 
+                console.log('Client closed'); 
+            }); 
+            
+            tmpclient.on('error', (err) => {
+                reject()
+                console.error(err); 
+            });
+        })
 
-    var createTransactionPacketJSON = JSON.stringify(createTransactionPacketForSignature);
+        await dataTransmitted;
 
-    createTransactionPacket.signature = signPacket(createTransactionPacketJSON).toBase64();
-
-    client.write(JSON.stringify(createTransactionPacket)); 
-    client.end();
-});
-
-client.on('data', (data) => {     
-    console.log(`Client received: ${data}`); 
-    if (data.toString().endsWith('exit')) { 
-        client.destroy(); 
     }
-}); 
-
-// Add a 'close' event handler for the client socket 
-client.on('close', () => { 
-    console.log('Client closed'); 
-}); 
-
-client.on('error', (err) => { 
-    console.error(err); 
-}); 
+}))
